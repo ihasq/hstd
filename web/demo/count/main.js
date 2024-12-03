701a6fdb6e951e2de831f2cbe0368ee74470337b
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-12-03 13:00:14 +0900

    wip

---
+++ web/demo/count/main.js
@@ -0,0 +1,17 @@
+import { $, h as html, on } from "../../libh.min.js";
+
+
+function Component() {
+
+	const count = $(0);
+
+	return html`
+		<h1>Count is ${count}</h1>
+		<button ${{ [on.click]: () => count.$++ }}>
+			Add more!
+		</button>
+	`;
+}
+
+
+document.body.append(...Component());
