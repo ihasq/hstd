41b1f7ea3a9e312eaa0655ab26ee8e23eca83a61
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-01-05 22:56:26 +0900

    wip

--- src/h.js
+++ src/h.js
@@ -1,12 +1,6 @@
 // ihasq/h ❤ lit-html's textEndRegex ❤
 
-let rand = Math.floor(Math.random() * (2 ** 32 - 1));
-
 const
-
-	ESC_REGEX = /["&'<>`]/g,
-	ESC_CHARCODE_BUF = {},
-	ESC_FN = (match) => `&#x${ESC_CHARCODE_BUF[match] ||= match.charCodeAt(0).toString(16)};`,
 
 	TOKEN_LENGTH = 16,
 
@@ -21,57 +15,68 @@ const
 
 	elementTempMap = new WeakMap(),
 
-	transformFrag = ({ s, v }, idList) => {
-		let elementTemp = elementTempMap.get(s);
+	transformFrag = ({ s, v, f }) => {
 
-		console.log(s, elementTemp?.node?.innerHTML)
+		let elementTemp = elementTempMap.get(s);
 
 		if(!elementTemp) {
 
 			let joined = s.join(""), tokenBuf;
-
 			while(joined.includes(tokenBuf = String.fromCharCode(...createToken()))) {};
-
 			joined = s.join(tokenBuf);
 
-			const attrMatch = Array.from(joined.matchAll(new RegExp(`<(?:(!--|\\/[^a-zA-Z])|(\\/?[a-zA-Z][^>\\s]*)|(\\/?$))[\\s].*?${tokenBuf}`, "g")).map(({ 0: { length }, index }) => index + length));
-			const placeholder = {};
+			const
+				attrMatch = Array.from(joined.matchAll(new RegExp(`<(?:(!--|\\/[^a-zA-Z])|(\\/?[a-zA-Z][^>\\s]*)|(\\/?$))[\\s].*?${tokenBuf}`, "g")).map(({ 0: { length }, index }) => index + length)),
+				placeholder = {},
+				tempDiv = document.createElement("div")
+			;
 
-			const tempDiv = document.createElement("div")
 			df.appendChild(tempDiv);
 
 			tempDiv.innerHTML = joined.replaceAll(tokenBuf, (match, index) => {
-				let id = String.fromCharCode(...createToken());
-
+				let id;
+				while(joined.includes(id = String.fromCharCode(...createToken()))) {};
 				return (placeholder[id] = attrMatch.includes(index + TOKEN_LENGTH)) ? id : `<br ${id}>` 
 			});
-
-			console.log(tempDiv)
 
 			elementTempMap.set(s, elementTemp = { node: tempDiv, placeholder })
 		};
 
-		const { node, placeholder } = elementTemp;
-		const newNode = node.cloneNode(true);
+		const
+			{ node, placeholder } = elementTemp,
+			newNode = node.cloneNode(true),
+			idList = {}
+		;
+
+		console.log(newNode.innerHTML)
+
 		Object.keys(placeholder).forEach((id, index) => {
-			const ref = newNode.querySelector(`[${id}]`);
-			const attrBody = v[index];
+
+			const
+				ref = newNode.querySelector(`[${id}]`),
+				attrBody = v[index]
+			;
+
 			if(placeholder[id]) {
+
 				Reflect.ownKeys(attrBody).forEach(attrProp => {
+
 					const
 						attrValue = attrBody[attrProp],
 						attrPropType = typeof attrProp
 					;
-					if(attrPropType === "symbol") {
+
+					if(attrPropType == "symbol") {
 
 						const ptr = globalThis[attrProp.description.slice(0, 52)]?.(attrProp);
 						if(!ptr?.[Symbol.toPrimitive]?.(PTR_IDENTIFIER)) return;
 						ptr.$(attrValue, ref);
 
-					} else if(attrPropType === "string") {
+					}
+					if(attrPropType == "string") {
 
 						if(attrValue[Symbol.toPrimitive]?.(PTR_IDENTIFIER)) {
-							if(attrProp === "value" && ref instanceof HTMLInputElement) {
+							if(attrProp == "value" && ref instanceof HTMLInputElement) {
 								const oninput = $ => ref.value = $;
 								attrValue.watch(oninput);
 								ref.addEventListener("input", ({ target: { value } }) => setTimeout(() => {
@@ -83,14 +88,14 @@ const
 								attrValue.watch($ => ref[attrProp] = $)
 							}
 
-						} else if(attrProp === "id" && !(attrValue in idList)) {
+						} else if(attrProp == "id" && !(attrValue in idList)) {
 							idList[attrValue] = ref;
 						} else {
 							ref[attrProp] = attrValue;
 						}
 					}
 				});
-				ref.removeAttribute(id)
+
 			} else {
 				const primitiveDef = attrBody[Symbol.toPrimitive];
 				if(primitiveDef?.(HTML_IDENTIFIER)) {
@@ -100,59 +105,25 @@ const
 					attrBody.watch($ => txt.textContent = $);
 					ref.replaceWith(txt)
 				} else {
-					ref.replaceWith(attrBody)
+					ref.previousSibling.textContent += attrBody + (ref.nextSibling?.textContent || "")
+					ref.nextSibling?.remove();
 				}
 			}
-		})
 
-		return newNode.childNodes;
-	},
+			ref.removeAttribute(id);
+		});
 
-	resolveFrag = ({ s, v }) => {
-		const profile = transformFrag({ s });
+		f?.(idList)
 
-	},
-
-	structFrag = ([s, v], str = [], val = [], marker, globalMarkerToken) => {
-		if(elementTempMap.has(s)) {
-			const { node, token, placeholder } = elementTempMap.get(s);
-			/**
-			 * 
-			 * 	{
-			 * 		"9wjsfd": "attribute"
-			 * 	}
-			 */
-			return {
-				cloned: node.cloneNode(true),
-				placeholder
-			}
-			thisEval.evaluate(elementTempMap.get(s).cloneNode(true));
-		}
-		const markerToken = String.fromCharCode(...createToken())
-		marker.push([s, markerToken])
-		str[str.length - 1] += s[0];
-		v.forEach((vBuf, vIndex) => {
-			if(vBuf[Symbol.toPrimitive]?.(HTML_IDENTIFIER)) {
-				structFrag(vBuf, str, val, marker, globalMarkerToken)
-				str[str.length - 1] += s[vIndex + 1]
-			} else if("number string".includes(typeof vBuf)) {
-				str[str.length - 1] += String(vBuf).replaceAll(ESC_REGEX, ESC_FN) + s[vIndex + 1]
-			} else {
-				val.push(vBuf)
-				str.push(s[vIndex + 1])
-			}
-		})
+		return newNode.children;
 	},
 
 	df = document.createDocumentFragment(),
 
-	thisEval = new XPathEvaluator(),
-
 	fragmentTemp = {
-		onloadCallbackFn: [],
 		then(onloadCallbackFn) {
-			if(!this.onloadCallbackFn.length) {
-				this.onloadCallbackFn[0] = onloadCallbackFn;
+			if(!this.f) {
+				this.f = onloadCallbackFn;
 			}
 			return this;
 		},
@@ -160,97 +131,11 @@ const
 			return typeof hint == "string" ? HTML_IDENTIFIER : hint === HTML_IDENTIFIER
 		},
 		[Symbol.iterator]: function* () {
-
 			for(const child of transformFrag(this)) yield child;
-
-			return;
-
-			const
-				str = [""],
-				val = [],
-				marker = [],
-				globalMarkerToken = String.fromCharCode(...createToken())
-			;
-
-			structFrag(this, str, val, marker, globalMarkerToken);
-
-			let joined = str.join(""), tokenBuf, hasId = false;
-
-			while(joined.includes(tokenBuf = String.fromCharCode(...createToken()))) {};
-
-			joined = str.join(tokenBuf);
-
-			const
-				attrMatch = Array.from(joined.matchAll(new RegExp(`<(?:(!--|\\/[^a-zA-Z])|(\\/?[a-zA-Z][^>\\s]*)|(\\/?$))[\\s].*?${tokenBuf}`, "g")).map(({ 0: { length }, index }) => index + length)),
-				attrIndex = [],
-				ptrIndex = [],
-				idList = {}
-			;
-			
-			let attrCount = -1;
-			tempDiv.innerHTML = joined.replaceAll(tokenBuf, (match, index) => (attrCount++, attrMatch.includes(index + TOKEN_LENGTH)
-				? (attrIndex.push(attrCount), match)
-				: (ptrIndex.push(attrCount), `<!--${tokenBuf}-->${tokenBuf}<!---->`)
-			));
-
-			const ptrMatch = thisEval.evaluate(`//comment()[contains(.,'${tokenBuf}')]`, tempDiv, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-
-			for(let i = 0; i < ptrIndex.length; i++) {
-				const comment = ptrMatch.snapshotItem(i);
-				comment.textContent = "";
-				const ptrText = comment.nextSibling;
-				val[ptrIndex[i]].watch($ => ptrText.textContent = $);
-			}
-
-			tempDiv.querySelectorAll(`[${tokenBuf}]`).forEach((ref, index) => {
-				const attrBody = val[attrIndex[index]];
-				Reflect.ownKeys(attrBody).forEach(attrProp => {
-					const
-						attrValue = attrBody[attrProp],
-						attrPropType = typeof attrProp
-					;
-					if(attrPropType === "symbol") {
-
-						const ptr = globalThis[attrProp.description.slice(0, 52)]?.(attrProp);
-						if(!ptr?.[Symbol.toPrimitive]?.(PTR_IDENTIFIER)) return;
-						ptr.$(attrValue, ref);
-
-					} else if(attrPropType === "string") {
-
-						if(attrValue[Symbol.toPrimitive]?.(PTR_IDENTIFIER)) {
-							if(attrProp === "value" && ref instanceof HTMLInputElement) {
-								const oninput = $ => ref.value = $;
-								attrValue.watch(oninput);
-								ref.addEventListener("input", ({ target: { value } }) => setTimeout(() => {
-									attrValue.ignore.set(oninput);
-									attrValue.$ = value
-									attrValue.ignore.delete(oninput);
-								}), { passive: true })
-							} else {
-								attrValue.watch($ => ref[attrProp] = $)
-							}
-
-						} else if(attrProp === "id" && !(attrValue in idList)) {
-							idList[attrValue] = ref;
-						} else {
-							ref[attrProp] = attrValue;
-						}
-					}
-				});
-				ref.removeAttribute(tokenBuf)
-			});
-
-			for(const tempNode of tempDiv.childNodes) yield tempNode;
-
-			this.onloadCallbackFn[0]?.(idList)
-
-			return;
 		}
 	},
 	h = (s, ...v) => Object.assign({ s, v }, fragmentTemp)
 ;
-
-// console.log(transformFrag(h`<div ${{ onclick: "ow" }}>${h`<div>wow</div>`}</div><input ${{}}><input ${{}}><input ${{}}>`))
 
 /**
  * 
