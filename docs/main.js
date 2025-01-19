7efaf3fb468daf939c76dacffd50ecacebe37525
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-01-19 14:32:31 +0900

    wip

--- docs/main.js
+++ docs/main.js
@@ -1,21 +1,80 @@
 import { $, h as html, on, css } from "libh";
 
-const Title = () => {
+/**
+ * 
+ * @param {{ src: string, alt: string, href: string }} param0 
+ * @returns 
+ */
+function Badge({ src, alt, href }) {
 	return html`
-		<h1>lib<a ${{ href: "https://libh.js.org" }}>h</a></h1>
-	`
+		<a ${{ href, target: "_blank", [css.textDecoration]: "none" }}><img ${{ src, alt }}></a>
+	`;
 }
 
-document.body.append(...html`
-	${Title()}
-	${Summary()}
-`)
-
-const Canvas = () => {
-	
+export default function() {
 	return html`
-		<canvas ${{ [on.load]: (canvas) => {
+		<div ${{
+			[css]: {
+				boxSizing: "border-box",
+				padding: "60px calc(20% - 60px)",
+				fontFamily: "int"
+			}
+		}}>
 
-		} }}></canvas>
-	`
+			<h1 ${{
+				[css.margin]: "40px 0",
+				[css.fontFamily]: 'jbm'
+			}}>
+				&lt;<a ${{ href: "https://libh.js.org", [css.textDecoration]: "none" }}>libh</a>&gt;
+			</h1>
+
+			<div ${{ [css.margin]: "0 0 20px" }}>
+				<a ${{ href: "https://github.com/ihasq/libh", target: "_blank" }}>GitHub</a>
+				<a ${{ href: "https://stackblitz.com/edit/vitejs-vite-vcga6uwx?file=main.js", target: "_blank" }}>StackBlitz</a>
+			</div>
+
+			<div ${{ [css.margin]: "0 0 20px" }}>
+				${Badge({
+					src: "https://img.shields.io/npm/v/libh?logo=npm&color=%23CC3534", 
+					alt: "npm version badge",
+					href: "https://npmjs.com/package/libh"
+				})}
+				${Badge({
+					src: "https://img.shields.io/bundlejs/size/libh?logo=stackblitz",
+					alt: "npm package minimized gzipped size",
+					href: "https://bundlephobia.com/package/libh"
+				})}
+			</div>
+
+			<div ${{ [css.margin]: "40px 0 0" }}>libh is a minimal JavaScript library to build simple, reactive, extensible web interface.</div>
+
+			<div ${{ [css.margin]: "60px 0" }}>
+				<h2 ${{ [css.fontFamily]: 'jbm' }}>Install</h2>
+
+				<code ${{
+					[css]: {
+						display: "block",
+						backgroundColor: "#02030f",
+						color: "#a2a3bf",
+						margin: "30px 0",
+						borderRadius: "4px",
+
+						padding: "16px 20px",
+						fontSize: "16px"
+					},
+					[on.click]() {
+						navigator.clipboard.writeText("npm i libh")
+					}
+				}}>$ <span ${{ [css.color]: "#f2f3ff" }}>npm i libh<span></code>
+			</div>
+
+			<div ${{ [css.margin]: "60px 0" }}>
+				<h2 ${{ [css.fontFamily]: 'jbm' }}>License</h2>
+
+				libh is <a ${{ href: "https://www.wtfpl.net" }}>WTFPL licensed</a>.
+			</div>
+
+
+		</div>
+	`;
 }
