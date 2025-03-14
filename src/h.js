6baa79e9493e27c6bfc1eed19a0589e944a2f0a2
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-03-15 00:16:31 +0900

    wip

--- src/h.js
+++ src/h.js
@@ -1,5 +1,3 @@
-// ihasq/h ❤ lit-html's textEndRegex ❤
-
 const
 
 	TOKEN_LENGTH = 16,
@@ -33,7 +31,7 @@ const
 				typeof targetValue == "function" &&
 				targetValue.toString().endsWith("() { [native code] }")
 			)
-				? (...args) => targetValue.apply(target, args)
+				? targetValue.bind(target)
 				: targetValue
 			;
 		}
@@ -51,132 +49,120 @@ const
 
 			const
 				attrMatch = Array.from(joined.matchAll(new RegExp(`<(?:(!--|\\/[^a-zA-Z])|(\\/?[a-zA-Z][^>\\s]*)|(\\/?$))[\\s].*?${tokenBuf}`, "g")).map(({ 0: { length }, index }) => index + length)),
-				placeholder = {},
+				placeholder = [],
 				node = document.createElement("div")
 			;
 
 			df.appendChild(node);
 
-			node.innerHTML = joined.replaceAll(tokenBuf, (_, index) => {
+			node.innerHTML = joined.replaceAll(tokenBuf, (_, index) => (placeholder[placeholder.length] = attrMatch.includes(index + TOKEN_LENGTH)) ? tokenBuf : `<br ${tokenBuf}>` );
 
-				let id;
-				while(joined.includes(id = String.fromCharCode(...createToken()))) {};
-				return (placeholder[id] = attrMatch.includes(index + TOKEN_LENGTH)) ? id : `<br ${id}>` 
-
-			});
-
-			elementTempMap.set(s, elementTemp = [node, placeholder])
+			elementTempMap.set(s, elementTemp = [node, placeholder, tokenBuf])
 		};
 
 		const
-			[node, placeholder] = elementTemp,
+			[node, placeholder, tokenBuf] = elementTemp,
 			newNode = node.cloneNode(true),
 			id = {}
 		;
 
-		Object.keys(placeholder).forEach((identifier, index) => {
+		newNode.querySelectorAll(`[${tokenBuf}]`).forEach((ref, index) => {
 
-			const
-				ref = newNode.querySelector(`[${identifier}]`),
-				vBody = v[index]
-			;
+			const vBody = v[index];
 
-			if(placeholder[identifier]) {
-
+			if(placeholder[index]) {
+	
 				Reflect.ownKeys(vBody).forEach(attrProp => {
-
+	
 					const
 						attrValue = vBody[attrProp],
 						attrPropType = typeof attrProp
 					;
-
-					// resolve prop
-
-					// let ptr;
-
+	
 					if(attrPropType == "symbol") {
-
+	
 						const attrPtr = globalThis[attrProp.description.slice(0, 52)]?.(attrProp);
-
+	
 						if(!attrPtr?.[Symbol.toPrimitive]?.(PTR_IDENTIFIER)) return;
-
+	
 						if(attrValue[Symbol.toPrimitive]?.(PTR_IDENTIFIER)) {
-
+	
 							attrValue.watch(newAttrValue => attrPtr.$(newAttrValue, ref))
-
+	
 						} else {
-
+	
 							attrPtr.$(attrValue, ref)
-
+	
 						}
-						// attrValue.watch(newValue => attrPtr.$(newValue, ref))
-						// attrPtr.$(attrValue, ref);
-
+	
 					} else if(attrPropType == "string") {
-
+	
 						if(attrValue[Symbol.toPrimitive]?.(PTR_IDENTIFIER)) {
-
+	
 							if(attrProp == "value" && attrProp in ref) {
-
+	
 								const oninput = $ => ref.value = $;
 								attrValue.watch(oninput);
-
+	
 								ref.addEventListener("input", ({ target: { value } }) => setTimeout(() => {
-
+	
 									attrValue.ignore.set(oninput);
 									attrValue.$ = value
 									attrValue.ignore.delete(oninput);
-
+	
 								}), { passive: true })
-
+	
 							} else {
-
+	
 								attrValue.watch(newAttrValue => ref[attrProp] = newAttrValue)
-
+	
 							}
-
+	
 						} else if(attrProp == "id" && !(attrValue in id)) {
-
+	
 							id[attrValue] = new Proxy(ref, refProxyHandler);
-
+	
 						} else {
-
+	
 							ref[attrProp] = attrValue;
-
+	
 						}
 					}
 				});
-
+	
 			} else {
-
+	
 				const primitiveDef = vBody[Symbol.toPrimitive];
-
+	
 				if(primitiveDef?.(HTML_IDENTIFIER)) {
-
+	
 					ref.replaceWith(...vBody);
-
+	
 				} else {
 					
 					const txt = new Text("")
 					ref.replaceWith(txt)
 					primitiveDef?.(PTR_IDENTIFIER) ? vBody.watch($ => txt.textContent = $) : txt.textContent = vBody
+	
+				}
+			}
+	
+			ref.removeAttribute(tokenBuf);
+		});
 
+		return Object.assign(
+
+			newNode.childNodes,
+			fragmentTemp,
+			{
+				then(onloadCallbackFn) {
+					onloadCallbackFn(id);
+					return this;
 				}
 			}
 
-			ref.removeAttribute(identifier);
-		});
-
-		return Object.assign(newNode.childNodes, fragmentTemp, {
-			then(onloadCallbackFn) {
-				onloadCallbackFn(id)
-				return this;
-			}
-		});
+		);
 	}
 ;
 
-/**
- * 
- */
 export { h }
